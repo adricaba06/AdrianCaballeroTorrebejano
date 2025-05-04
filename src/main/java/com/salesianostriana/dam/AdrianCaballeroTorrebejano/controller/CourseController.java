68c267e7a3fdd3360b677d28ae1893a559957271
adricaba06 <caballero.toadr24@triana.salesianos.edu>
2025-05-04 18:00:14 +0200

    cambios en formulario de creación de alumnos

--- src/main/java/com/salesianostriana/dam/AdrianCaballeroTorrebejano/controller/CourseController.java
+++ src/main/java/com/salesianostriana/dam/AdrianCaballeroTorrebejano/controller/CourseController.java
@@ -1,9 +1,13 @@
 package com.salesianostriana.dam.AdrianCaballeroTorrebejano.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.AdrianCaballeroTorrebejano.model.Course;
 import com.salesianostriana.dam.AdrianCaballeroTorrebejano.service.CourseService;
@@ -16,10 +20,22 @@ public class CourseController {
 	
 	//Controller para mostrar una lista 
 	
-	@GetMapping("/")
-	public String showDashboard (Model model) {
-		model.addAttribute("courses", courseService.listAll());
-		return "dashboard";
+	@GetMapping("/dashboard")
+	public String showDashboard(Model model) {
+	    model.addAttribute("course", new Course()); 
+
+	    List<Course> courses = courseService.listAll(); 
+	    model.addAttribute("courses", courses); 
+
+	    return "dashboard";
 	}
+	
+	  @PostMapping("/newcourse")
+	    public String createCourse(@ModelAttribute("course") Course course) {
+	        courseService.save(course); 
+	        return "redirect:/dashboard"; 
+	    }
+	
+	
 
 }
--- src/main/java/com/salesianostriana/dam/AdrianCaballeroTorrebejano/model/Course.java
+++ src/main/java/com/salesianostriana/dam/AdrianCaballeroTorrebejano/model/Course.java
@@ -25,10 +25,10 @@ public class Course {
 
 	private String name;
 	private String description;
-	private String profesor;
+	private String teacher;
 
 	private int maxCapacity;
-	private int numOfStudents;
+	private int numOfStudents = 0;
 	
 	@Enumerated(EnumType.STRING)
 	private Level level;
@@ -40,9 +40,104 @@ public class Course {
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private LocalDate endDate;
 
-	private boolean active;
+	private boolean active = true;
 
 	private String imageUrl;
+	
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
+	public int getNumOfStudents() {
+		return numOfStudents;
+	}
+
+	public void setNumOfStudents(int numOfStudents) {
+		this.numOfStudents = numOfStudents;
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Course() {
+		super();
+	}
+
+	
+	
 	
 }
